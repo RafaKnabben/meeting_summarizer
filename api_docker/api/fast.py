46be22032456a5a7f75d6043fb061ba971e32bbd
Kohei Suzuki <koheisuzuki@me.com>
2021-11-18 21:31:03 +0100

    add modified files

--- api_docker/api/fast.py
+++ api_docker/api/fast.py
@@ -1,29 +1,44 @@
-'''
-need to be improved !!!!!
-'''
-
+# import joblib
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 # define a root '/' endpoint
 @app.get("/")
 def index():
-    # load pipeline
+
+    # ⚠️ TODO: get model from GCP
+    # pipeline = get_model_from_gcp()
+    # pipeline = joblib.load('model.joblib')
+
     return {"ok": True}
 
-
-# make speech recognition (transcription) & summarization
-@app.get("/summarize")
-def summarize(*args, **kwargs):
-    # run pipline
+# transcription
+@app.get("/transcribe")
+def transcribe(*args, **kwargs):
+    #run pipline
     pass
 
+# summarization
+@app.get("/summarize")
+def summarize(*args, **kwargs):
+    #run pipline
+    pass
 
 # get keywords
 @app.get("/keywords")
 def get_keywords(*args, **kwargs):
-    # generate keywords from transcription/summarization
+    # generate keywords from summarization
     pass
