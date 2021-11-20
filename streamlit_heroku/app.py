d24ce499e7c82ddbc879584f6a3b9843880ab449
Sana Shah <shahsana@hotmail.com>
2021-11-20 11:44:18 +0100

    First draft website

---
+++ streamlit_heroku/app.py
@@ -0,0 +1,30 @@
+import streamlit as st
+
+'''
+# Speech Summarizer
+'''
+
+st.markdown('''Our application allows users to create a summary of a given speech on video or audio.
+            Simply use the features below and it will output an abstractive summary from your given media input.
+
+''')
+
+'''
+## Submit your url:
+
+url == 'path'
+
+## Upload your file:
+
+url == 'path'
+
+'''
+
+'''
+## Our model will now work create a summary transcript of your media input.
+
+'''
+
+"""
+## Here is your summary
+"""
