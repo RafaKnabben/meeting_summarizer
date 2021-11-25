1736624ee23e209b71eadcc61973ec0c2d974e94
Kohei Suzuki <koheisuzuki@me.com>
2021-11-25 09:19:20 +0100

    ver2411

--- api_docker/api/fast.py
+++ api_docker/api/fast.py
@@ -9,26 +9,25 @@ from pydub import AudioSegment
 from deepspeech import Model
 from scipy.io.wavfile import read as wav_read
 from transformers import T5Tokenizer, T5ForConditionalGeneration, T5Config
-from nemo.collections.nlp.models import PunctuationCapitalizationModel
+import nemo
+import nemo.collections.nlp as nemo_nlp
 import torch
-
 
 app = FastAPI()
 
 # params for Deepspeech
-model_file_path = 'deepspeech-0.9.3-models.pbmm'
-lm_file_path = 'deepspeech-0.9.3-models.scorer'
+model_file_path = './deepspeech-0.9.3-models.pbmm'
+lm_file_path = './deepspeech-0.9.3-models.scorer'
 beam_width = 100
 lm_alpha = 0.93
 lm_beta = 1.18
 
 # params for T5
-num_beams=4
-no_repeat_ngram_size=2
-min_length=30
-max_length=100
-early_stopping=True
-
+num_beams = 4
+no_repeat_ngram_size = 2
+min_length = 30
+max_length = 100
+early_stopping = True
 
 #CORS
 app.add_middleware(
@@ -40,20 +39,12 @@ app.add_middleware(
 )
 
 
-#define a root '/' endpoint
-@app.get("/")
-def index():
-    # ⚠️ TODO: get model from GCP
-    # pipeline = get_model_from_gcp()
-    # pipeline = joblib.load('model.joblib')
-    return {"ok": True}
-
-
-#download youtube video
-@app.post("/download")
+# functions
+# download youtube video
 def get_tube(url):
     ydl_opts = {
-        'format': 'bestaudio/best',
+        'format':
+        'bestaudio/best',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'mp3',
@@ -65,81 +56,116 @@ def get_tube(url):
         info_dict = ydl.extract_info(url, download=False)
         video_title = info_dict.get('id', None)
 
-    path = f'{video_title}.mp3'
-
-    ydl_opts.update({'outtmpl':path})
+    mp3 = f'{video_title}.mp3'
+    ydl_opts.update({'outtmpl': mp3})
 
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
-    return path
+    return mp3
 
 
-#extract audio data from youtube video
-@app.get("/extract")
-def get_audio(url):
-    path =  f"/content/{get_tube(url)}"
-    shortcut = path[:-4]
-    path_wav = f"{shortcut}.wav"
+# extract audio data from youtube video
+def get_audio(mp3):
+    shortcut = mp3[:-4]
+    wav = f"{shortcut}.wav"
 
-    sound = AudioSegment.from_file(path)
-    sound.export(path_wav, format="wav")
+    sound = AudioSegment.from_file(mp3)
+    sound.export(wav, format="wav")
 
-    os.remove(path)
+    os.remove(mp3)
 
-    audio, sr = librosa.load(path_wav, sr=16000)
-    soundfile.write(path_wav, data = audio, samplerate = sr)
+    audio, sr = librosa.load(wav, sr=16000)
+    soundfile.write(wav, data=audio, samplerate=sr)
 
-    return path_wav
+    return wav
 
 
 # transcription
-@app.get("/transcribe")
-def get_transcript(url):
+def get_transcript(wav):
     model = Model(model_file_path)
     model.enableExternalScorer(lm_file_path)
     model.setScorerAlphaBeta(lm_alpha, lm_beta)
     model.setBeamWidth(beam_width)
 
-    path_wav = get_audio(url)
-    rate, buffer= wav_read(path_wav)
+    rate, buffer = wav_read(wav)
     transcript = model.stt(buffer)
 
-    os.remove(path_wav)
+    os.remove(wav)
 
     return transcript
 
 
 # summarization
-@app.get("/summarize")
-def summarize(url):
-    transcript = get_transcript(url)
+def get_summary(transcript):
+    model_1 = nemo_nlp.models.PunctuationCapitalizationModel.from_pretrained(
+        model_name="punctuation_en_bert")
+    transcript = model_1.add_punctuation_capitalization([transcript])
 
-    PunctuationCapitalizationModel.list_available_models()
-    model = PunctuationCapitalizationModel.from_pretrained("punctuation_en_bert")
-    transcript = model.add_punctuation_capitalization([transcript])
-
-    model = T5ForConditionalGeneration.from_pretrained('t5-base')
+    model_2 = T5ForConditionalGeneration.from_pretrained('t5-base')
     tokenizer = T5Tokenizer.from_pretrained('t5-base')
     device = torch.device('cpu')
 
-
-    t5_prepared_Text = "summarize: "+transcript[0]
-    tokenized_text = tokenizer.encode(t5_prepared_Text, return_tensors="pt").to(device)
-    summary_ids = model.generate(tokenized_text,
-                                 num_beams=num_beams,
-                                 no_repeat_ngram_size=no_repeat_ngram_size,
-                                 min_length=min_length,
-                                 max_length=max_length,
-                                 early_stopping=early_stopping)
+    t5_prepared_Text = "summarize: " + transcript[0]
+    tokenized_text = tokenizer.encode(t5_prepared_Text,
+                                      return_tensors="pt").to(device)
+    summary_ids = model_2.generate(tokenized_text,
+                                   num_beams=num_beams,
+                                   no_repeat_ngram_size=no_repeat_ngram_size,
+                                   min_length=min_length,
+                                   max_length=max_length,
+                                   early_stopping=early_stopping)
 
     output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    summary = {"Summarized text": output}
 
-    return print("\n\nSummarized text: \n",output)
+    return summary
+
+
+# endpoints
+#define a root '/'
+@app.get("/")
+def index():
+    return {"ok": True}
+
+
+@app.post("/download_test")
+def get_tube_only(url):
+    mp3 = get_tube(url)
+    return mp3
+
+
+@app.get("/extract_test")
+def get_audio_only(url):
+    mp3 = get_tube_only(url)
+    wav = get_audio(mp3)
+    return wav
+
+
+@app.get("/transcribe_test")
+def get_transcript_only(url):
+    wav = get_audio_only(url)
+    transcript = get_transcript(wav)
+    return transcript
+
+
+@app.get("/summarize_test")
+def get_summary_only(url):
+    transcript = get_transcript_only(url)
+    summary = get_summary(transcript)
+    return summary
+
+
+@app.get("/all_steps_test")
+def get_all(url):
+    mp3 = get_tube(url)
+    wav = get_audio(mp3)
+    transcript = get_transcript(wav)
+    summary = get_summary(transcript)
+    return summary
 
 
 # # get keywords from summary
 # @app.get("/keywords")
 # def get_keywords(*args, **kwargs):
 #     pass
-###
