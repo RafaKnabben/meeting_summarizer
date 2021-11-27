b42258d2093398ccb7304ca819adb96702df0d00
Kohei Suzuki <koheisuzuki@me.com>
2021-11-27 07:06:05 +0100

    modified rate

--- api_docker/api/fast.py
+++ api_docker/api/fast.py
@@ -48,7 +48,7 @@ def get_tube(url):
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'wav',
-            'preferredquality': '16',
+            'preferredquality': '16k',
         }],
     }
 
