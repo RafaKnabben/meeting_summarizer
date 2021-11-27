1bd3ca65f79666836bc1cdeb7cb856f22d64e12b
Kohei Suzuki <koheisuzuki@me.com>
2021-11-27 06:59:31 +0100

    minor bit

--- api_docker/api/fast.py
+++ api_docker/api/fast.py
@@ -48,7 +48,7 @@ def get_tube(url):
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': 'wav',
-            'quality': '16',
+            'audio-quality': '16k',
             'preferredquality': '192',
         }],
     }
